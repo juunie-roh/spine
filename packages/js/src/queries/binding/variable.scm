de010ff2b01ffb479bac208c49851c42e7af5eb4
Juun <blacklily7513@gmail.com>
2026-05-06 15:32:48 +0900

    fix(plugin): Remove variable regex filtering
    The core graph node addition includes deduplication logic

--- packages/js/src/queries/binding/variable.scm
+++ packages/js/src/queries/binding/variable.scm
@@ -4,25 +4,12 @@
 (lexical_declaration
   [("const") ("let")] @kind
   (variable_declarator
-    name: (_) @name
-    value: (_)? @v (#not-match? @v "^[(]?[ \t\n]*(function|class|require|import|await import|[(][^)]*[)][ \t\n]*=>)"))
+    name: (_) @name)
 ) @node
 
 ;; @kind = "var"
 (variable_declaration
   ("var") @kind
   (variable_declarator
-    name: (_) @name
-    value: (_)? @v (#not-match? @v "^[(]?[ \t\n]*(function|class|require|import|await import|[(][^)]*[)][ \t\n]*=>)"))
+    name: (_) @name)
 ) @node
-
-;; Rust regex
-;; "^[(]?[ \t\n]*(function|class|require|import|await import|[(][^)]*[)][ \t\n]*=>)"
-;; function ...
-;; (...) => ...
-;; class ...
-;; (function ...)
-;; (() => ...)
-;; require ...
-;; import ...
-;; await import ...
