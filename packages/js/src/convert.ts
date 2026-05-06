7b8552f22d4c2e7d3d635d03dda14ec3dc5327f7
Juun <blacklily7513@gmail.com>
2026-05-06 15:51:25 +0900

    refactor(plugin): Classify iife as scope, not binding

--- packages/js/src/convert.ts
+++ packages/js/src/convert.ts
@@ -2,23 +2,26 @@ import ifHandler from "./handlers/anonymous/if";
 import iifeHandler from "./handlers/anonymous/iife";
 import whileHandler from "./handlers/anonymous/while";
 import esmBindingHandler from "./handlers/binding/esm";
-import iifeBindingHandler from "./handlers/binding/iife";
 import memberHandler from "./handlers/binding/member";
 import variableHandler from "./handlers/binding/variable";
 import classHandler from "./handlers/scope/class";
 import functionHandler from "./handlers/scope/function";
+import iifeScopeHandler from "./handlers/scope/iife";
 import methodHandler from "./handlers/scope/method";
 import type { ConvertConfig } from "./types";
 
 export const convertConfig: ConvertConfig = {
+  // anonymous
   if: ifHandler,
   "iife.anonymous": iifeHandler,
   while: whileHandler,
+  // binding
   "esm.binding": esmBindingHandler,
-  "iife.binding": iifeBindingHandler,
   member: memberHandler,
   variable: variableHandler,
+  // scope
   class: classHandler,
   function: functionHandler,
+  "iife.scope": iifeScopeHandler,
   method: methodHandler,
 } as const;
